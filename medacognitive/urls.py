1dd34698e694958a6aa37623966d86add2edef06
Ruocheng Wang <yrrejkk@gmail.com>
2018-02-24 12:41:31 -0500

    Created Superuser

--- medacognitive/urls.py
+++ medacognitive/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import home, hospital
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hospital/', hospital),
+    path('/',home)
 ]
