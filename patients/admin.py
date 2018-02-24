4662e4eb20a03e9c8ec7d83120fcfe6de1f13ce8
CaptainSkyVenom <zhiwei_gan@hotmail.com>
2018-02-24 15:22:36 -0500

    test

--- patients/admin.py
+++ patients/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Patient)
+admin.site.register(Survey)
--- patients/models.py
+++ patients/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class Patient(models.Model):
     first_name=models.CharField("First Name",max_length=30)
     last_name=models.CharField("Last Name",max_length=30)
-    med_names = models.TextField("Current Medications")
-    med_time = models.TextField("Time to take meds")
-    med_left = models.TextField("How Much Left")
-    meds_taken = models.TextField("Meds Taken", null=True)
+    med = models.TextField("Medicine")
+
+
+class Survey(models.Model):
+    patient = models.OneToOneField(Patient, on_delete=models.CASCADE)
+    med_name = models.TextField("Med name")
+    med_comment = models.TextField("Comment")
