4de70bbba612fa70c64753b0e6cb545c0e4efd27
CaptainSkyVenom <zhiwei_gan@hotmail.com>
2018-02-24 13:30:10 -0500

    test

--- doctors/models.py
+++ doctors/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    first_name = models.CharField("First Name", max_length=30)
+    last_name = models.CharField("Last Name", max_length=30)
+    patient_list = models.TextField("List of Patients")
--- patients/models.py
+++ patients/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 
-
 class Patient(models.Model):
     first_name=models.CharField("First Name",max_length=30)
     last_name=models.CharField("Last Name",max_length=30)
+    med_names = models.TextField("Current Medications")
+    med_time = models.TextField("Time to take meds")
+    med_left = models.TextField("How Much Left")
